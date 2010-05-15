7f53582dbb7217b80e123be2b1b85639bdef9cbc
alwinmathew316 <alwinmathew316@6af25ba2-7ab4-5b61-25f6-3ebd0582b9a4>
2010-05-15 12:40:54 +0000

    Modified login & registration page

--- login.php
+++ login.php
@@ -36,6 +36,7 @@
 	#sideline a
 	{
 		text-decoration: none;
+                cursor: pointer;
 		color: #1272c1;
 	}
 	#sideline a:visited
@@ -129,7 +130,7 @@
 	{
 		background: url('images/blue_invert.gif') repeat-x left top;
 	}
-	#response
+	#error_login, #error_signup
 	{
 		font-family: "Trebuchet MS", Helvetica, sans-serif;
 		color: red;
@@ -137,77 +138,168 @@
 		font-weight: bold;
 		text-align: center;
 	}
+
+        #popup
+        {
+                top: 202px; /*202px;*/
+                left: 536px; /*536px;*/
+                border: 2px solid teal;
+                background-color: #b8dbe6;
+		width: 200px;
+		margin-left: auto;
+		margin-right: auto;
+		padding: 81px;
+                padding-top: 26px;
+		height:307px;
+                position: fixed;
+        }
+
+        #popup button
+        {
+                width: 160px;
+                margin-top: 15px;
+		margin-left: 21px;
+		height: 40px;
+		background: url('images/blue.gif') repeat-x left top;
+		font-family: "Trebuchet MS", Helvetica, sans-serif;
+		font-size: 14px;
+		color: white;
+		border-width: 0px;
+       	}
+	#popup button:hover
+	{
+		background: url('images/blue_invert.gif') repeat-x left top;
+	}
+        
+        #popup_close
+        {
+                width:13px;
+                height:13px;
+                position:relative;
+                cursor:pointer;
+                background-color: teal;
+                left:250px;
+                text-align:center;
+                top:-15px;
+                border: 1px solid red;
+        }
 	</style>
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript">
+
+                $(document).ready(function(){
+                        $("#popup").hide();
+                	$("#login_form").submit(function(){
+                                var username=$("#login_username").val();
+                                var password=$("#login_password").val();
+                                if(username==""&&password=="")
+                                        $("#error_login").html("Username & password cannot be blank!");
+                                else if(username=="")
+                                        $("#error_login").html("Username cannot be blank!");
+                                else if(password=="")
+                                        $("#error_login").html("Password cannot be blank!");
+                                else
+                                {
+                                        $.ajax({
+                                                type: "POST",
+                                                url: "checklogin.php",
+                                                data: "myusername=" +username +"&mypassword=" +password,
+                                                success: function(data){
+                                                        if(data=="success")
+                                                                $("#error_login").html("Login Successful!");
+                                                        else
+                                                                $("#error_login").html("Invalid Login! Try again.");
+                                                }
+                                        });
+                                }
+                                return false;
+                        });
+                        $("#register").click(function(){
+                                $("#page").fadeTo("fast",0.1);
+                                $("#popup").show();
+                        });
+                        $("#popup_close").click(function(){
+                                $("#popup").hide();
+                                $("#signup_username").val("");
+                                $("#signup_password").val("");
+                                $("#error_signup").html("");
+                                $("#page").fadeTo("fast",1);
+                        });
+                        $("#signup_form").submit(function(){
+                                var username=$("#signup_username").val();
+                                var password=$("#signup_password").val();
+                                if(username==""&&password=="")
+                                        $("#error_signup").html("Username & password cannot be blank!");
+                                else if(username=="")
+                                        $("#error_signup").html("Username cannot be blank!");
+                                else if(password=="")
+                                        $("#error_signup").html("Password cannot be blank!");
+                                else
+                                {
+                                        $.ajax({
+                                                type: "POST",
+                                                url: "addmember.php",
+                                                data: "myusername=" +username +"&mypassword=" +password,
+                                                success: function(data){
+                                                        if(data=="success")
+                                                                $("#error_signup").html("Success! You may login now.");
+                                                        else
+                                                                $("#error_signup").html("Username already exists! Use a different name.");
+                                                }
+                                        });
+                                }
+                                return false;
+                        });
+		});
+
+	</script>
 
 </head>
 
 <body>
 
-	<script type="text/javascript">
-
-	function loadXMLDoc()
-	{
-		if(window.XMLHttpRequest)
-		{// code for IE7+, Firefox, Chrome, Opera, Safari
-			xmlhttp=new XMLHttpRequest();
-		}
-		else
-		{// code for IE6, IE5
-			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		xmlhttp.onreadystatechange=function()
-		{
-			if (xmlhttp.readyState==4 && xmlhttp.status==200)
-			{
-				document.getElementById("response").innerHTML=xmlhttp.responseText;
-			}
-		}
-		var username=document.getElementById("myusername").value;
-		var password=document.getElementById("mypassword").value;
-		if(username==""&&password=="")
-			xmlhttp.open("GET","err_3.txt",true);
-		else if(username=="")
-			xmlhttp.open("GET","err_1.txt",true);
-		else if(password=="")
-			xmlhttp.open("GET","err_2.txt",true);
-		else
-			return true;
-		xmlhttp.send();
-		return false;
-	}
-
-	</script>
-
+	
+	
 	<div id="page">
 		<div id="header">
-			<div id="sideline"><a href="login.php" title="Login to your account">Login</a> | <a href="register.php" title="Register as new user">Register</a></div>
+			<div id="sideline"><a href="login.php" title="Login to your account">Login</a> | <a id="register" title="Register as new user" onclick='$("#popup").show();'>Register</a></div>
 			<div id="title"><a href="main.html">Resume-Bakery</a></div>
 			<div id="tagline">Enter a tagline here</div>
 		</div>
 
-		<div id="login_box"><form name="form1" method="post" action="checklogin.php" onsubmit="return loadXMLDoc()"><div id="table">
+            <div id="login_box"><form id="login_form"><div id="table">
 			<table align="center">
 			<th>Member Login</th>
-			<tr>
-			<td>Username</td>
-			</tr>
-			<tr>
-			<td><input name="myusername" type="text" id="myusername"></td>
-			</tr>
+			<tr><td>Username</td></tr>
+			<tr><td><input name="myusername" type="text" id="login_username"></td></tr>
 			<tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
-			<tr>
-			<td>Password</td>
-			</tr>
-			<tr>
-			<td><input name="mypassword" type="password" id="mypassword"></td>
-			</tr>
+			<tr><td>Password</td></tr>
+			<tr><td><input name="mypassword" type="password" id="login_password"></td></tr>
 			<tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr></table></div>
-			<button type="submit">Login</button>
+			<button id="login_button" type="submit">Login</button>
 			</form>
 			<p><a href="">Forgot password?</a></p>
-			<div id="response"> </div>
+			<div id="error_login"> </div>
 		</div>
 	</div>
+
+        <div id="popup">
+                <div id="popup_close"></div>
+                <div id="sign_up" style='font-family: "Trebuchet MS", Helvetica, sans-serif;'>
+                    <form id="signup_form">
+                        <table align="center">
+                        <th style="padding-bottom: 30px; font-size: 18px;">New Account?</th>
+			<tr><td style="font-size: 14px; width: 50px;">Username</td></tr>
+                        <tr><td style="width: 50px;"><input name="myusername" type="text" id="signup_username"></td></tr>
+			<tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+			<tr><td style='font-size: 14px; width: 50px;'>Password</td></tr>
+			<tr><td><input name="mypassword" type="password" id="signup_password"></td></tr>
+                        <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr></table>
+        		<button id="signup_button" type="submit">Register</button>
+                        </form>
+                        <p id="error_signup"></p>
+                </div>
+        </div>
 
 </body>
 
