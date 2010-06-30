9451a2d7def58bca9b51bc4317a2ae36af823b3f
alwinmathew316 <alwinmathew316@6af25ba2-7ab4-5b61-25f6-3ebd0582b9a4>
2010-06-30 18:58:16 +0000

    Corrected error during registration

--- addmember.php
+++ addmember.php
@@ -20,7 +20,7 @@
 	mysql_connect("$host", "$username", "$password")or die("cannot connect");
 	mysql_select_db("$db_name")or die("cannot select DB");
 
-        $sql="SELECT * FROM $tbl_name WHERE username='$myusername'";
+        $sql="SELECT * FROM $user_tbl WHERE username='$myusername'";
 	$result=mysql_query($sql);
 
 	// Mysql_num_row is counting table row
