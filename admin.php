de742fc118ee60ea8bdcd153a3905f76a3f2c38d
alwinmathew316 <alwinmathew316@6af25ba2-7ab4-5b61-25f6-3ebd0582b9a4>
2010-07-14 13:59:07 +0000

    Modified installer

--- admin.php
+++ admin.php
@@ -66,33 +66,9 @@
                 closedir($handle);
                 rmdir($src);
         }
-//	function delete_directory($dirname)
-//	{
-//		if (is_dir($dirname))
-//			$dir_handle = opendir($dirname);
-//   		if (!$dir_handle)
-//			return false;
-//		while($file = readdir($dir_handle))
-//		{
-//			if ($file != "." && $file != "..")
-//			{
-//   				if (!is_dir($dirname."/".$file))
-//					unlink($dirname."/".$file);
-//				else
-//					delete_directory($dirname.'/'.$file);
-//			}
-//  		}
-//		closedir($dir_handle);
-//		rmdir($dirname);
-//		return true;
-//	}
-
-        mover("install",".");
-//	delete_directory("install");
-
-//        system("mv install/* .");
-//        system("rm -rf install");
+        system("mv install/* .");
+        system("rm -rf install");
         rmdir("install");
-//        unlink("installer.php");
+        unlink("installer.php");
         return ("success");
 ?>
